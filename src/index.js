02ba3aca8b7953feefe4f62d1cc8222832a3c6f3
Eliseo Arévalo <2480931@clases.edu.sv>
2023-02-10 13:52:59 -0600

    [:sparkles: init configs]

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+require('dotenv').config()
+
+const app = express()
+
+app.get("/", (req, res)=>{
+  res.send("Hello word")
+})
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
